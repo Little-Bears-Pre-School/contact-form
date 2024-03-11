bce45cf0662585147cb070414b43f6a2ae0b0157
Andy Driver <andyhd@users.noreply.github.com>
2024-03-11 17:38:38 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Contact Form
+
+A Google Cloud Function script which processes a HTML form submission and sends
+the form data in an email via Mailgun to hello@little-bears.com
---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+import functions_framework
+import httpx
+from flask import redirect
+
+
+@functions_framework.http
+def contact_form(request):
+    sender_email = request.form['email']
+    sender_name = f"{request.form['first_name']} {request.form['last_name']}"
+    body = request.form['message']
+
+    httpx.post(
+        f'https://api.mailgun.net/v3/{os.getenv("MAILGUN_DOMAIN_NAME")}/messages',
+        auth=('api', os.getenv('MAILGUN_API_KEY')),
+        data={
+            'from': f'{sender_name} <{sender_email}>',
+            'to': 'hello@little-bears.com',
+            'subject': 'Website Contact Form Submission',
+            'text': f'{body}\n\nTel: {request.form["phone"]}',
+        },
+    )
+
+    return redirect('https://little-bears.com/contact-success', code=302)
