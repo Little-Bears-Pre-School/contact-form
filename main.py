920a0e8b2252cf053adbac316419300ba84a9213
Andy Driver <andy@pagezero.net>
2024-03-14 00:03:50 +0000

    Add a honeypot field
    * We already had some spam
    * Bots will fill all fields, humans will not see the honeypot field
    * We can ignore requests from bots

--- main.py
+++ main.py
@@ -7,19 +7,23 @@ from flask import redirect
 
 @functions_framework.http
 def contact_form(request):
-    sender_email = request.form['email']
+    # only bots will fill in this honeypot field
+    if request.form.get("middle_name"):
+        return redirect("https://little-bears.com/contact-success", code=302)
+
+    sender_email = request.form["email"]
     sender_name = f"{request.form['first_name']} {request.form['last_name']}"
-    body = request.form['message']
+    body = request.form["message"]
 
     httpx.post(
         f'https://api.mailgun.net/v3/{os.getenv("MAILGUN_DOMAIN_NAME")}/messages',
-        auth=('api', os.getenv('MAILGUN_API_KEY')),
+        auth=("api", os.getenv("MAILGUN_API_KEY")),
         data={
-            'from': f'{sender_name} <{sender_email}>',
-            'to': 'hello@little-bears.com',
-            'subject': 'Website Contact Form Submission',
-            'text': f'{body}\n\nTel: {request.form["phone"]}',
+            "from": f"{sender_name} <{sender_email}>",
+            "to": "hello@little-bears.com",
+            "subject": "Website Contact Form Submission",
+            "text": f'{body}\n\nTel: {request.form["phone"]}',
         },
     )
 
-    return redirect('https://little-bears.com/contact-success', code=302)
+    return redirect("https://little-bears.com/contact-success", code=302)
